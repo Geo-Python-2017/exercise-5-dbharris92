ea17b7ec2fffcec885ef27ef3bed2aef988f71b5
dbharris92 <harris.db92@gmail.com>
2018-02-28 16:23:18 -0500

    typo

--- data_exploration.py
+++ data_exploration.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Exercise 5, Problem 1
-
-Exploring NOAA data
+Exercise 5 Exploring NOAA data
 
 Part 1 of this script prints mean Farenheit temperature, SD of maximum, and number of unique stations
 
 Part 2 of this script converts selected F temps to C
 
+Part 3 of this script divides datas into subsets and saves them to file
+ 
 db harris 2.28.18 
 """
 
@@ -54,4 +54,20 @@ selected['Celsius'] = (selected['TEMP']- 32) / 1.8
 ### round Celsius values to zero decimals and convert to integer
 selected['Celsius'] = selected['Celsius'].round(0)
 
+### Problem 3 ###
+
+### select all rows from selected DF where USAF code is 299980
+kumpula = selected.ix[selected['USAF'] == 29980]
+
+### select rows where USAF is 28450
+rovaniemi = selected.ix[selected['USAF'] == 28450]
+
+### save these dataframes to csv
+output1 = 'Kumpula_temps_May_Aug_2017.csv'
+output2 = 'Rovaniemi_temps_May_Aug_2017.csv'
+
+kumpula.to_csv(output1, sep= ',', float_format='%.2f')
+rovaniemi.to_csv(output2, sep = ',', float_format='%.2f')
+
+### Problem 4 ###
 
