f4964885de27bae3e336306e174127f9d3251478
dbharris92 <harris.db92@gmail.com>
2018-02-28 21:57:52 -0500

    solved 4, some progress on 5

--- data_exploration.py
+++ data_exploration.py
@@ -7,9 +7,17 @@ Part 1 of this script prints mean Farenheit temperature, SD of maximum, and numb
 Part 2 of this script converts selected F temps to C
 
 Part 3 of this script divides datas into subsets and saves them to file
+
+Part 4.1 calculates median temperature for helsinki Kumpula and Rovaniemi stations
+Part 4.b calculates median temperature for May and June separetely for H-K and Rovaniemi stations
+
+Part 5
  
 db harris 2.28.18 
 """
+#################
+### Problem 1 ###
+#################
 
 ### import pandas
 import pandas as pd
@@ -31,7 +39,9 @@ print('The std dev of maximum temp is', dF['MAX'].std())
 ### print unique number of unique stations
 print('There are', dF['USAF'].nunique(), 'unique stations.')
 
+#################
 ### Problem 2 ###
+#################
 
 ### select USAF, YR--MODAHRMN, TEMP, MAX, MIN columns
 selected = dF[['USAF', 'YR--MODAHRMN', 'TEMP', 'MAX', 'MIN']]
@@ -54,7 +64,9 @@ selected['Celsius'] = (selected['TEMP']- 32) / 1.8
 ### round Celsius values to zero decimals and convert to integer
 selected['Celsius'] = selected['Celsius'].round(0)
 
+#################
 ### Problem 3 ###
+#################
 
 ### select all rows from selected DF where USAF code is 299980
 kumpula = selected.ix[selected['USAF'] == 29980]
@@ -69,5 +81,39 @@ output2 = 'Rovaniemi_temps_May_Aug_2017.csv'
 kumpula.to_csv(output1, sep= ',', float_format='%.2f')
 rovaniemi.to_csv(output2, sep = ',', float_format='%.2f')
 
+#################
 ### Problem 4 ###
+#################
 
+### calculate median temperature for H-Kumpula and Rovaniemi stations
+print('The median temperature in Kumpula was', kumpula['TEMP'].median(), 'degrees Fahrenheit.')
+print('The median temperature in Rovaniemi was', rovaniemi['TEMP'].median(), 'degrees Fahrenheit.')
+
+### select May temperatures for kumpula and rovaniemi
+kumpula_may = kumpula.ix[(kumpula['YR--MODAHRMN'] >= 201705010000) & (kumpula['YR--MODAHRMN'] <= 201705312300)]
+rovaniemi_may = rovaniemi.ix[(rovaniemi['YR--MODAHRMN'] >= 201705010000) & (rovaniemi['YR--MODAHRMN'] <= 201705312300)] 
+
+### select June temperatures for kumpula and rovaniemi
+kumpula_june = kumpula.ix[(kumpula['YR--MODAHRMN'] >= 201706010000) & (kumpula['YR--MODAHRMN'] <= 201706302300)]
+rovaniemi_june = rovaniemi.ix[(rovaniemi['YR--MODAHRMN'] >= 201706010000) & (rovaniemi['YR--MODAHRMN'] <= 201706302300)] 
+
+### print these results                              
+print('The median temperature in Kumpula in May was', kumpula_may['TEMP'].median(), 'degrees Fahrenheit.')
+print('The median temperature in Rovaniemi in May was was', rovaniemi_may['TEMP'].median(), 'degrees Fahrenheit.')
+print('The median temperature in Kumpula in June was', kumpula_june['TEMP'].median(), 'degrees Fahrenheit.')
+print('The median temperature in Rovaniemi in June was was', rovaniemi_june['TEMP'].median(), 'degrees Fahrenheit.')                             
+
+
+
+#################
+### Problem 5 ###
+#################
+###link for help:http://benalexkeen.com/resampling-time-series-data-with-pandas/
+### create new dataframe
+
+resample_k = kumpula
+resample_k.is_copy = None
+
+
+### create better timestamps
+resample_k['YR--MODAHRMN'] = resample_k['YR--MODAHRMN'].date_range('2017-01-01', '2015-12-31', freq='1H')  
